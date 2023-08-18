91c5aea989c9cf510fc35ffc00ac5ee1cb650058
Monica Qiu <m1102778586@gmail.com>
2023-08-17 19:53:49 -0600

    added album detail page with formatted date

--- Spotify/ViewControllers/Others/AlbumViewController.swift
+++ Spotify/ViewControllers/Others/AlbumViewController.swift
@@ -11,6 +11,50 @@ class AlbumViewController: UIViewController {
     
     private let album: Album
     
+    private var viewModels = [AlbumCollectionViewCellViewModel]()
+    
+    private var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ in
+            
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 2, bottom: 1, trailing: 2)
+            
+            // Group
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(60)
+                ),
+                subitem: item,
+                count: 1
+            )
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: group)
+            
+            // Section header
+            section.boundarySupplementaryItems = [
+                NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .fractionalWidth(1.0)
+                    ),
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+            ]
+            
+            return section
+        })
+    )
+    
     init(album: Album) {
         self.album = album
         super.init(nibName: nil, bundle: nil)
@@ -22,21 +66,110 @@ class AlbumViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = album.name
         view.backgroundColor = .systemBackground
         
-        APICaller.shared.getAlbumDetails(for: self.album) { result in
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(
+            AlbumTrackCollectionViewCell.self,
+            forCellWithReuseIdentifier: AlbumTrackCollectionViewCell.identifier
+        )
+        collectionView.register(
+            PlaylistHeaderCollectionReusableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier
+        )
+        
+        APICaller.shared.getAlbumDetails(for: album) { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let albumDetails):
-                    break
+                    self?.viewModels = albumDetails.tracks.items.compactMap({ detail in
+                        AlbumCollectionViewCellViewModel(
+                            name: detail.name,
+                            artistName: detail.artists.first?.name ?? "-"
+                        )
+                    })
+                    self?.collectionView.reloadData()
                 case .failure(let error):
                     print(error.localizedDescription)
                     break
                 }
             }
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+}
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: AlbumTrackCollectionViewCell.identifier,
+            for: indexPath
+        ) as? AlbumTrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configure(with: viewModels[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier,
+            for: indexPath
+        ) as? PlaylistHeaderCollectionReusableView,
+              kind == UICollectionView.elementKindSectionHeader
+        else {
+            return UICollectionReusableView()
+        }
+        
+        let headerViewModel = PlaylistHeaderViewModel(
+            name: album.name,
+            ownerName: album.artists.first?.name,
+            description: "Release Date: \(String.formattedDate(string: album.release_date))",
+            artworkURL: URL(string: album.images.first?.url ?? "")
+        )
+        header.configure(with: headerViewModel)
+        header.delegate = self
+        
+        return header
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        // xxx play song
+    }
+    
+}
+
+// MARK: - Header Delegate
+extension AlbumViewController: PlaylistHeaderCollectionReusableViewDelegate {
+    func didTapPlayAll(_ header: PlaylistHeaderCollectionReusableView) {
+        print("play all")
     }
 
 }
---
+++ Spotify/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Monica Qiu on 8/17/23.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
